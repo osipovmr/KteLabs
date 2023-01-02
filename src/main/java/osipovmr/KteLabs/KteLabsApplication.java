139679eab89540abcc01a70adda07c17baf7626d
osipovmr <osipovmr@yandex.ru>
2023-01-02 16:24:53 +0600

    initial commit

---
+++ src/main/java/osipovmr/KteLabs/KteLabsApplication.java
@@ -0,0 +1,13 @@
+package osipovmr.KteLabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KteLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KteLabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/osipovmr/KteLabs/KteLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package osipovmr.KteLabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KteLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
